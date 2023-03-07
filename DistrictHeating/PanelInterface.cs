13c2fc11560914f7f50e300d516fd199805e62ea
Gerhard Hofmann <ghofmann@cadability.de>
2023-03-07 20:31:58 +0100

    latest changes

---
+++ DistrictHeating/PanelInterface.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Text;
+
+namespace DistrictHeating
+{
+    public interface IPanel
+    {
+        IGraphics CreateGraphics();
+    }
+
+    public interface IGraphics: IDisposable
+    {
+        void Clear(int color);
+        void DrawLine(int color, float x1, float y1, float x2, float y2);
+    }
+}
